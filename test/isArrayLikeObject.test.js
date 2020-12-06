fc78426b33269a48de0ecb767166e81d1cd272f8
Markus Kylänpää <markus.kylanpaa@tuni.fi>
2020-12-06 07:05:06 +0200

    Added test cases for function isArrayLikeObject

---
+++ test/isArrayLikeObject.test.js
@@ -0,0 +1,39 @@
+import chai from 'chai';
+const expect = chai.expect;
+import isArrayLikeObject from '../src/isArrayLikeObject.js';
+
+describe('IsArrayLikeObject', () => {
+    describe('Basic Functionality', () => {
+        const arr = [1, 2, 3]
+        const obj = {
+            'a': 1,
+            'b': 2
+        }
+        function arrLike() {
+            this.push('test');
+        }
+        arrLike.prototype = [];
+        const arrLikeObject = new arrLike();
+
+        it('Testing a normal array', () => {
+            const result = isArrayLikeObject(arr);
+            expect(result).to.equal(true);
+        });
+        it('Testing an array-like object', () => {
+            const result = isArrayLikeObject(arrLikeObject);
+            expect(result).to.equal(true);
+        });
+        it('Testing a normal object', () => {
+            const result = isArrayLikeObject(obj);
+            expect(result).to.equal(false);
+        });
+        it('Testing a string', () => {
+            const result = isArrayLikeObject('abc');
+            expect(result).to.equal(false);
+        });
+        it('Testing a function', () => {
+            const result = isArrayLikeObject(Function);
+            expect(result).to.equal(false);
+        });
+    });
+});
